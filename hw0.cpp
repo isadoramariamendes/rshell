ebabdf009e772e551352b008de31993cff0df5c7
Isadora Souza <isadoramariamendes@live.com>
2015-04-17 03:14:04 -0700

    .cpp and Makefile add again under the right branch

--- hw0.cpp
+++ hw0.cpp
@@ -6,47 +6,98 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <vector>
+#include <string>
+#include <sys/types.h>
+#include <pwd.h>
 using namespace std;
 
-vector<int> conn_order;													//vector to track order and type of connectors
+vector<int> conn_order;
 int words = 0;
 
-bool isExit(char *cmd){
-    if (strcmp("exit", cmd)) return false;
-    return true;
+/*
+ *  POSTCONDITION: Prints the username and hostname, if possible.
+ */
+void userInfo() {
+    char machine[100];
+    struct passwd *password = getpwuid(getuid());
+    int hostname = gethostname(machine, 100);
+    
+    if(password != NULL && hostname != -1) {
+        string username = password->pw_name;
+        cout << username << "@" << machine << " ";
+    }
+    cout << "$ ";
 }
 
-bool isEmpty(char *cmd){
-    if(strlen(cmd) == 0) return true;
-    return false;
+/*
+ *  POSTCONDITION: Prints the username and hostname, if possible.
+ *  OUTPUT: a string of chars with all commands.
+ */
+char *getCommands() {
+    string commandLine;
+    
+    getline(cin, commandLine);
+    if (commandLine == "exit") exit(0);
+    char *commands = new char [commandLine.size() + 1];
+    strcpy(commands, commandLine.c_str());
+    
+    return commands;
 }
 
+/*
+ *  INPUT: char *cmd - holds a command.
+ *  OUTPUT: True if it's a comment, or false if it's not.
+ */
 bool isComment(char *cmd){
     if(cmd[0] == '#') return true;
     return false;
 }
 
-size_t cmdSize (char *cmd) {
+/*
+ *  INPUT: char *cmd - holds a command.
+ *  OUTPUT: The size of the command.
+ */
+size_t cmdize(char *cmd) {
     return strlen(cmd);
 }
 
-void disp_v(vector<int> &v){
-    for(unsigned i=0; i<v.size(); ++i){
-        cout << v.at(i);
-    }
-    cout << '\n';
-}
-
-void count_words(char *cmds){
-    for(unsigned i=0; i<conn_order.size(); ++i){
-        if(conn_order.at(i) == -1) ++words;
-    }
-}
-
-char tok_comment(char *cmd){
+/*
+ *  POSTCONDITION: Remove the comments from the command line.
+ */
+void tok_comment(char *cmd){
     cmd = strtok(cmd, "#");
 }
 
+/*
+ *  INPUT: char *cmd - holds a command without comments already
+ *  POSTCONDITION: Loops inside of the command line, char by char and
+ *  pushes back some labels into a vector, whose value depends
+ *  on the connector found.
+ */
+void read_order(char *cmd){
+    for(int i = 0; cmd[i] != '\0'; ++i){
+        if (cmd[i] == ';') {
+            conn_order.push_back(1);
+        }
+        else if (cmd[i] == '&' && cmd[i + 1] == '&') {
+            conn_order.push_back(2);
+            ++i;
+        }
+        else if (cmd[i] == '|' && cmd[i + 1] == '|'){
+            conn_order.push_back(3);
+            ++i;
+        }
+    }
+}
+
+/*
+ *  INPUT:
+ *  char **cmdlist - array of strings which will carry each individual command with its respective arguments;
+ *  char *cmd - holds a command line that can have many other commands chained together using the "&& || ;" operators
+ *  OUTPUT: The number of "argument-lines".
+ *  POSTCONDITION: Everytime that the connectos "&& || ;" be found
+ *  cmdline[x] will have that command and the number of commands will be increased by 1.
+ */
 int tok_conn(char **cmdlist, char *cmd){
     int argc = 0;
     cmdlist[argc] = strtok(cmd, ";&|");
@@ -57,16 +108,34 @@ int tok_conn(char **cmdlist, char *cmd){
     return argc;
 }
 
-void tok_space(char **cmdlist, int size){
+/*
+ *  INPUT: char *cmd - holds a command.
+ *  OUTPUT: True if it's exit, or false if it's not.
+ */
+bool isExit(char *c){
+    if (!strcmp(c, "exit")) {
+        //cout << "BYE!" << endl;
+        return true;
+    }
+    return false;
+}
+
+/*
+ *  INPUT: **cmdlist - holds the lines with commands whose connectors were tokenized.
+ *  int size - value is the number of arguments calculated to exist between all connectors.
+ *  POSTCONDITION: Loops inside the function tokenizing an entire argument line's 
+ *  white space and organizes those tokens to be executed with 'execvp()'
+ */
+void tok_space (char **cmdlist, int size) {
     int curr = 0;
     unsigned index = 0;
-    char **temp;
-    temp = new char *[words + 1];
-    while(curr < size){
+    char **temp = new char *[words + 1];
+    
+    while (curr != size) {
         int argc = 0;
         temp[argc] = strtok(cmdlist[curr], " ");
-        while(temp[argc] != NULL){
-            if(isExit(temp[argc])){
+        while (temp[argc] != NULL) {
+            if (isExit(temp[argc])) {
                 exit(0);
             }
             ++argc;
@@ -75,28 +144,28 @@ void tok_space(char **cmdlist, int size){
         argc = 0;
         
         int pid = fork();
-        if(pid == -1){
+        if (pid == -1) {
             perror("fork");
         }
-        else if(pid == 0){
+        else if (pid == 0) {
             int ret = execvp(temp[argc], temp);						//There is some black magic happening here
-            if(ret == -1){
+            if (ret == -1) {
                 perror("execvp");
             }
             exit(1);
         }
-        else{
+        else {
             int ret;
             waitpid(pid,&ret,0);
-            if(conn_order.size() > 0 && index < conn_order.size()){
-                if(ret != 0){
-                    if(conn_order.at(index) == 2){
+            if (conn_order.size() > 0 && index < conn_order.size()) {
+                if(ret != 0) {
+                    if(conn_order.at(index) == 2) {
                         return;
                     }
                     ++index;
                 }
-                else{
-                    if(conn_order.at(index) == 3){
+                else {
+                    if(conn_order.at(index) == 3) {
                         return;
                     }
                     ++index;
@@ -107,77 +176,21 @@ void tok_space(char **cmdlist, int size){
     }
 }
 
-/*
- *  OUTPUT: char *cmd - holds a command line.
- *   POSTCONDITION: Allocates memory to each char read from the command line.
- *    Puts '\0' at the end to mark end of commanding.
- *  
- */
-char *readCmd() {
-    
-    char *cmd = (char*) malloc(sizeof(char));
-    char c;
-    
-    int i = 0;
-    int j = 1;
-    
-    printf("$ ");
-    while(c != '\n')
-    {
-        c = getc(stdin);
-        cmd = (char*)realloc(cmd,j*sizeof(char));
-        cmd[i] = c;
-        i++;
-        j++;
-    }
-    cmd[i]='\0';
-    
-    return cmd;
-}
-
-/*
- *  INPUT: char *cmd - holds a command without comments already
- *   POSTCONDITION: Loops inside of the command line, char by char and
- *    pushes back some labels into a vector, whose value depends
- *     on the connector found.
-*/
-void readOrder(char *cmd){
-    for (int i = 0; cmd[i] != '\0'; ++i){
-        if(cmd[i] == ';'){
-            conn_order.push_back(1);
-        }
-        else if(cmd[i] == '&' && cmd[i+1] == '&'){
-            conn_order.push_back(2);
-            ++i;
-        }
-        else if(cmd[i] == '|' && cmd[i+1] == '|'){
-            conn_order.push_back(3);
-            ++i;
-        }
-    }
-}
-
 int main()
 {
-    char *cmd = 0;
-    while(1){
+    while(1) {
         
-        cmd = readCmd(); 
-        if(cmd == "exit") exit(0);
+        userInfo();
+        char *cmd = getCommands();
         
-        if(!isEmpty(cmd) && !isComment(cmd)){
-            
-            char ** cmdlist = new char *[cmdSize(cmd)];
+        if (!isComment(cmd)) {
+            char **cmdlist = new char *[cmdize(cmd) + 1];
             tok_comment(cmd);
-            readOrder(cmd);
-            //display_v(conn_order);
+            read_order(cmd);
             int list_size = tok_conn(cmdlist,cmd);
             tok_space(cmdlist, list_size);
-                                 
         }
-        words = 0;
-        conn_order.clear(); 
+        conn_order.clear();
     }
-    free(cmd);
     return 0;
-} 
+}
